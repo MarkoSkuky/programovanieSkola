c1b1d85df45bd8dfa29319210efe5fb0a98529a5
Marko <markoskukalek1@gmail.com>
2022-09-06 10:40:15 +0200

    upload01

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import javafx.scene.transform.Scale;
+
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
-}
+    public static void main(String[] args) {
+        System.out.println("ja sa volam marko ako sa volas ty ");
+        String slovo;
+        Scanner scanner = new Scanner(System.in);
+        slovo = scanner.next();
+        if (Objects.equals(slovo, "marko")) {
+            System.out.println("waau aj ty si marko");
+        }
+        }
+    }
+
