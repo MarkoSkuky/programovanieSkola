ef574c52e85d76768aa06caced379d8644e44646
Marko <markoskukalek1@gmail.com>
2022-09-06 11:04:58 +0200

    upload01

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
 import javafx.scene.transform.Scale;
 
 import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ja sa volam marko ako sa volas ty ");
-        String slovo;
-        Scanner scanner = new Scanner(System.in);
-        slovo = scanner.next();
-        if (Objects.equals(slovo, "marko")) {
-            System.out.println("waau aj ty si marko");
+        int[] pole = new int[10];
+        int najmensie = 999999999;
+
+        Random random = new Random();
+
+        for (int i = 0; i < pole.length; i++){
+            pole[i] = random.nextInt(100);
         }
+        for (int i = 0; i < pole.length; i++){
+            System.out.println(pole[i]);
         }
+
+         for (int i = 0; i < pole.length; i++){
+             if (pole[i] < najmensie){
+                 najmensie = pole[i];
+             }
+         }
+        System.out.println("najmensie cislo v poli je: "+najmensie);
+
+
     }
+
+
+
+
+
+
+    }
+
 
