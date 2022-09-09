190fcd1c90fea8ac7b1394e8d301715f7c76ed15
Marko <markoskukalek1@gmail.com>
2022-09-09 10:41:21 +0200

    Opakovanie

---
+++ src/Opakovanie.java
@@ -0,0 +1,73 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Opakovanie {
+    public static void main(String[] args) {
+        //01
+//        Scanner scanner= new Scanner(System.in);
+//        Random random = new Random();
+//
+//        int cislo = scanner.nextInt();
+//        if (cislo >= 20 && cislo <= 30){
+//            System.out.println("Patri do roymedzia");
+//        }else if (cislo < 20){
+//            System.out.println("cislo sa nachadza pod rozmedzim");
+//        }else System.out.println("cislo sa nachadza nad rozmedzim");
+
+        //02
+//        for (int i = 1; i <= 1000;i++){
+//
+//            if (i % 2 == 0){
+//                System.out.println(i + " parne");
+//            }else System.out.println(i + " neparne");
+//        }
+
+        //03
+//        int cislo1 = 0;
+//        for (int i = 0; i <= 9; i++) {
+//            for (int o = 0; o < i; o++) {
+//                System.out.print("-");
+//            }
+//            System.out.print(i + "" + i);
+//
+//
+//            for (int o = 0; o < i; o++) {
+//                System.out.print("-");
+//
+//            }
+//            System.out.println();
+//        }
+
+
+        //04
+//        int cislo2 = 1;
+//        while (cislo2 != 6){
+//            System.out.println("cislo " + cislo2);
+//            cislo2++;
+//        }
+
+
+        //05
+//        String[][] pole = new String[5][5];
+//        String text = "Prazdne";
+//        for (int riadok = 0; riadok < pole.length; riadok++) {
+//            for (int stlpec = 0; stlpec < pole.length; stlpec++) {
+//                pole[riadok][stlpec] = text;
+//            }
+//        }
+//        pole[1][4] = "zilina";
+//        pole[0][2] = "mexiko";
+//        pole[3][0] = "bytca";
+//
+//
+//        for (int riadok = 0; riadok < pole.length; riadok++) {
+//            for (int stlpec = 0; stlpec < pole.length; stlpec++) {
+//                System.out.print(pole[riadok][stlpec] +" ");
+//            }
+//            System.out.println();
+//        }
+
+
+    }
+}
+
