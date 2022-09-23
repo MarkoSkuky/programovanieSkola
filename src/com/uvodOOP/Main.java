5c17dee4833368fdaa09b60c43417705442b6c32
Marko <markoskukalek1@gmail.com>
2022-09-23 10:02:26 +0200

    Objektoveprogramovanie

---
+++ src/com/uvodOOP/Main.java
@@ -0,0 +1,8 @@
+package com.uvodOOP;
+
+public class Main {
+    public static void main(String[] args) {
+        Osoba osoba = new Osoba("Marko", "Skukalek", "889622/4895");
+        Osoba osoba1 = new Osoba("Filip", "Šulgan", "848894/5478");
+    }
+}
---
+++ src/com/uvodOOP/Osoba.java
@@ -0,0 +1,15 @@
+package com.uvodOOP;
+
+public class Osoba {
+    private String meno;
+    private String priezvisko;
+    private String rodCislo;
+
+    public Osoba(String Meno, String priezvisko, String rodCislo) {
+        this.meno = meno;
+        this.priezvisko = priezvisko;
+        this.rodCislo = rodCislo;
+    }
+
+
+}
